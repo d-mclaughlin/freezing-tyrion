a887568f8f4514468cd580db1a519f3779d76759
Laurynas Mince <laurynas.mince@gmail.com>
2015-02-17 13:28:21 +0000

    New version of evolve -- seems to work

--- main.h
+++ main.h
@@ -48,11 +48,62 @@ public:
   
   // Evolve takes a given point on the grid and transforms it into the average of the 4 points around it
   // We need the old grid to find the new grid point, and the relaxation factor too.
+  /*
   void evolve(Grid *old, int x, int y, float relaxation) {
     // This relies on there being 4 points surrounding this point, which is not always the case
     //  TODO(david): Edge and corner cases
     this->set(x, y, (1 - relaxation) * old->get(x,y) + (relaxation / 4) *
-      old->get(x, (y+1)) + old->get(x, (y-1)) + old->get((x-1), y) + old->get((x-1), y));
+      old->get(x, (y+1)) + old->get(x, (y-1)) + old->get((x-1), y) + old->get((x+1), y));
   }
+  */
+
+  // LAURYNAS: VERSION OF EVOLVE WHICH SEEMS TO WORK
+  void evolve(Grid *old, Grid *is_fixed, int row, int col, float relaxation) {
+    if (is_fixed->voltages[row * cols + col] == 1) {
+      return;
+    }
+    // Top left corner
+    else if (row == 0 && col == 0) {
+      this->voltages[row * cols + col] = old->voltages[(row+1) * cols + col];
+    }
+    // Top right corner
+    else if (row == 0 && col == (cols-1)) {
+      this->voltages[row * cols + col] = old->voltages[(row+1) * cols + col];
+    }
+    // Bottom left corner
+    else if (row == (rows-1) && col == 0) {
+      this->voltages[row * cols + col] = old->voltages[(row-1) * cols + col];
+    }
+    // Bottom right corner
+    else if (row == (rows-1) && col == (cols-1)) {
+      this->voltages[row * cols + col] = old->voltages[(row-1) * cols + col];
+    }
+    // Top row
+    else if (row == 0) {
+      this->voltages[row * cols + col] = old->voltages[(row+1) * cols + col];
+    }
+    else if (row == 0) {
+      this->voltages[row * cols + col] = old->voltages[(row+1) * cols + col];
+    }
+    // Bottom row
+    else if (row == (rows-1)) {
+      this->voltages[row * cols + col] = old->voltages[(row-1) * cols + col];
+    }
+    else if (row == (rows-1)) {
+      this->voltages[row * cols + col] = old->voltages[(row-1) * cols + col];
+    }
+    // Left side
+    else if (col == 0) {
+      this->voltages[row * cols + col] = old->voltages[row * cols + (col+1)];
+    }
+    // Right side
+    else if (col == (cols-1)) {
+      this->voltages[row * cols + col] = old->voltages[row * cols + (col-1)];
+    }
+    else {
+      this->voltages[row * cols + col] = (1 - relaxation) * old->get(row,col) + ( relaxation / 4.0) * (old->get(row, (col+1)) + this->get(row, (col-1)) + this->get((row-1), col) + old->get((row+1), col));
+    }
+  }
+  
 };
 #endif
